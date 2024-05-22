abba868c32f3766e21c4ad6e4a87b180314548d6
Javier Payan <42685814+javierpayan97@users.noreply.github.com>
2024-05-21 19:15:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,15 +13,11 @@ def ask_number(min_number,max_number):
                 print(f"Error: please enter a number between {min_number} and {max_number}")
     return number_int
 
-def random_number(min_number,max_number):
-    random_n = random.randint(min_number,max_number)
-    return random_n
-
 min_number=0
 max_number=10
 number = 0
 lifes = 4
-magic_number = random_number(min_number,max_number)
+magic_number = random.randint(min_number,max_number)
 while lifes != 0:
     number = ask_number(min_number,max_number)
     if int(number) == magic_number:
